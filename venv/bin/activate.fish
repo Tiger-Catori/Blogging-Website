a18f4324bc5a26f7e244b4d237599fe8d2d3b850
Tiger-Catori <michaeladeleye365@gmail.com>
2026-03-13 20:54:02 +0000

    1st commit from new PC adding some tweaks and refactoring.

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/duro/Documents/Coding Projects/PythonProjects/Blogging Website/venv'
+set -gx VIRTUAL_ENV '/home/duro/Documents/Coding Projects/BloggingWebsite/Blogging-Website/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
